b8b063ce659ce4f170b349e5ce2c9dd035b565a1
inatatsu-tatsuhiro <tatsuhiro.9699@gmail.com>
2022-07-26 15:44:48 +0900

    :+1: iikanji

--- src/Greeter.ts
+++ src/Greeter.ts
@@ -1,5 +1,22 @@
 export class Greeter {
   greet(to: string): void {
-    console.log(`Hello ${to}`);
+    console.log(`Hello ${to}`)
   }
+}
+
+export interface SSSWindow extends Window {
+  SSS: {
+    activeName: string
+    activeAddress: string
+    activePublicKey: string
+    activeNetworkType: number
+    getActiveAccountToken: (
+      verifierPublicKey: string,
+      customPayload?: Object,
+      encriptedPayload?: string
+    ) => Promise<string>
+    setTransaction: (transaction: any) => void
+    requestSign: () => Promise<any>
+  }
+  isAllowedSSS: () => boolean
 }
